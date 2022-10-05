532cee6433f3ae8c80ecf0fc4ab0c2939ae5f075
lomicster <lomicster@gmail.com>
2022-10-05 00:18:58 -0700

    Start katas resolutions

--- Python/lesson3.2.py
+++ Python/lesson3.2.py
@@ -53,7 +53,8 @@ count_by(2,5) #should return [2,4,6,8,10]"""
 def count_by(x, n):
     list(range(x, x * n + 1, x))
 
-"""4. https://www.codewars.com/kata/52223df9e8f98c7aa7000062
+
+'''4. https://www.codewars.com/kata/52223df9e8f98c7aa7000062
 How can you tell an extrovert from an introvert at NSA? Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
 I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it? According to Wikipedia, ROT13 (http://en.wikipedia.org/wiki/ROT13) is frequently used to obfuscate jokes on USENET.
 Hint: For this task you're only supposed to substitue characters. Not spaces, punctuation, numbers etc.
@@ -62,5 +63,45 @@ Test examples:
 "EBG13 rknzcyr." -->
 "ROT13 example."
 "This is my first ROT13 excercise!" -->
-"Guvf vf zl svefg EBG13 rkprepvfr!""""
+"Guvf vf zl svefg EBG13 rkprepvfr!"'''
 
+# def rot13(message):
+#   return message.encode('rot13')
+def rot13(message):
+    abc = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM'
+    r = ''
+    for c in message:
+        if c in abc:
+            r += abc[abc.index(c) + 13]
+        else:
+            r += c
+    return r
+
+"""HW1. https://www.codewars.com/kata/5da9973d06119a000e604cb6
+You need count how many valleys you will pass.
+Start is always from zero level.
+Every time you go down below 0 level counts as an entry of a valley, and as you go up to 0 level from valley counts as an exit of a valley.
+One passed valley is equal one entry and one exit of a valley.
+s='FUFFDDFDUDFUFUF'
+U=UP
+F=FORWARD
+D=DOWN
+To represent string above
+
+(level 1)  __
+(level 0)_/  \         _(exit we are again on level 0)
+(entry-1)     \_     _/
+(level-2)       \/\_/
+So here we passed one valley"""
+
+
+
+"""HW2. Написать функцию/метод user_friendly_size:
+-принимает на вход число (в байтах)
+-возвращает float (2 decimal precision) в user friendly size (Kb, Mb, Gb, Tb)
+-using test up to you (pytest or print with fn)
+-challenge: pass the file path to the fn and fn shoud return "human" size
+
+Examples:
+user_friendly_size(9999)=>9.76Kb
+user_friendly_size(9999999)=>9.54Mb"""
---
+++ Python/lesson5.2.py
@@ -0,0 +1,58 @@
+"""Variables
+https://www.codewars.com/kata/50ee6b0bdeab583673000025/python
+a = "code"
+b = "wa.rs"
+name = a + b
+https://www.codewars.com/kata/5859d3325a8a0747180000c6/train/python
+def hello(name):
+   return(f'Hello, {name}')
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+def greet(name):
+    return(f'Hello, {name} how are you doing today?')
+"""
+
+"""Type casting
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+def number_to_string(num):
+    return str(num)
+https://www.codewars.com/kata/544675c6f971f7399a000e79
+def string_to_number(s):
+    return int(s)
+https://www.codewars.com/kata/563d59dd8e47a5ed220000ba/train/python
+def get_sum_of_digits(num):
+    sum = 0
+    digits = str(num)
+    for x in digits:
+        sum+=int(x)
+    return sum
+"""
+
+"""strings
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/solutions/python
+https://www.codewars.com/kata/57f759bb664021a30300007d/train/python
+https://www.codewars.com/kata/5b180e9fedaa564a7000009a/solutions/python
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018/python
+
+booleans
+https://www.codewars.com/kata/56606694ec01347ce800001b/python
+https://www.codewars.com/kata/55afed09237df73343000042/train/python
+
+if//else
+https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/python
+https://www.codewars.com/kata/5810085c533d69f4980001cf/train/python
+https://www.codewars.com/kata/5ad0d8356165e63c140014d4/train/python
+https://www.codewars.com/kata/55225023e1be1ec8bc000390/python
+https://www.codewars.com/kata/5a6d3bd238f80014a2000187/train/python
+
+for loops
+https://www.codewars.com/kata/5865918c6b569962950002a1/train/python
+https://www.codewars.com/kata/56b1f01c247c01db92000076/train/python
+
+range
+https://www.codewars.com/kata/598057c8d95a04f33f00004e/solutions/python
+https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/python
+
+lists
+https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/python
+https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/python
+https://www.codewars.com/kata/580435ab150cca22650001fb/python"""
