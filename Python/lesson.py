997725f3c1c93a9bb769ceb6629b09c79c4794c7
lomicster <lomicster@gmail.com>
2022-10-17 18:49:55 -0700

    links added to homework

---
+++ Python/lesson.py
@@ -0,0 +1,12 @@
+import time
+from webbrowser import BaseBrowser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdrive_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome (ChromeDriverManager().install())
+driver.get('http://suninjuly.github.io/cats.html')
+
+time.sleep(5)
+bullet_cat = driver.find_element(By.XPATH, '/html/body/main/div/div/div/div[1]/div/div/div/div/button[1]').click
+time.sleep(5)
