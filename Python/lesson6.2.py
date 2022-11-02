b5631367fa17535c78cfebeb2cf8143e431d3e04
lomicster <lomicster@gmail.com>
2022-11-01 23:21:20 -0700

    Added lesson 6 with solutions

---
+++ Python/lesson6.2.py
@@ -0,0 +1,43 @@
+"""1. https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/python"""
+
+"""rooms = {
+1: {'name': 11, 'description': 21, 'completed': 31},
+2: {'name': 11, 'description': 21, 'completed': 31},
+3: {'name': 11, 'description': 21, 'completed': 31}
+}"""
+
+"""2. https://www.codewars.com/kata/53f0f358b9cb376eca001079/python
+
+class Ball(object):
+    def __init__(self, ball_type='regular'):
+        self.ball_type = ball_type"""
+
+"""3. https://www.codewars.com/kata/57126304cdbf63c6770012bd/python
+def isDigit(string):
+    try:
+        float(string)
+        return True
+    except:
+        return False"""
+
+"""4. https://www.codewars.com/kata/56a4addbfd4a55694100001f/python
+def validate_hello(greetings):
+    return any(x in greetings.lower() for x in ['hello','ciao','salut','hallo','hola','ahoj','czesc'])
+
+
+import re
+def validate_hello(greetings):
+    return bool(re.search('ciao|hello|salut|hallo|hola|ahoj|czesc', greetings.lower()))"""
+
+"""5. https://www.codewars.com/kata/57faece99610ced690000165/python
+def remove(s):
+    return s.rstrip("!")
+
+import re
+
+def remove(s):
+    return re.sub('!*$', '', s)"""
+
+
+Homework: https://www.codewars.com/kata/52597aa56021e91c93000cb0/python
+
