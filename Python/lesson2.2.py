1c4f4ba986cc0d6c6955d1d858ef4909865af90b
lomicster <lomicster@gmail.com>
2023-08-29 22:58:31 -0700

    Codewars 2.2 update

---
+++ Python/lesson2.2.py
@@ -0,0 +1,44 @@
+##For-While Loops. If-Else statements##
+#1 https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+def make_upper_case(s):
+    return s.upper()
+
+#2 https://www.codewars.com/kata/5601409514fc93442500010b
+def better_than_average(class_points, your_points):
+    sum = 0
+    for point in class_points:
+        sum += point
+    class_avg = sum / len(class_points)
+    return your_points > class_avg
+
+#3 https://www.codewars.com/kata/5808e2006b65bff35500008f
+def position(alphabet):
+    return(f"Position of alphabet: {ord(alphabet)-96}")
+
+#4 https://www.codewars.com/kata/5302d846be2a9189af0001e4
+def say_hello(name, city, state):
+    return (f'Hello, {" ".join(name)}! Welcome to {city}, {state}!')
+
+#5 https://www.codewars.com/kata/55225023e1be1ec8bc000390
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    else:
+        return "Hello, {name}!".format(name=name)
+#6 https://www.codewars.com/kata/563b74ddd19a3ad462000054
+
+https://www.codewars.com/kata/56c22c5ae8b139416c00175d
+https://www.codewars.com/kata/557b5e0bddf29d861400005d
+https://www.codewars.com/kata/56b29582461215098d00000f
+https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+https://www.codewars.com/kata/57280481e8118511f7000ffa
+https://www.codewars.com/kata/57241e0f440cd279b5000829
+https://www.codewars.com/kata/58261acb22be6e2ed800003a
+https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+https://www.codewars.com/kata/5875b200d520904a04000003
+
+
+
+https://www.codewars.com/kata/57eae65a4321032ce000002d
+https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
